85144a68064e2567ab9a5484584f76d5bb46e16c
Maj Frost Jensen <maj@frost-jensen.dk>
2021-09-27 11:59:45 +0200

    Added delegates to main method in program

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -6,7 +6,21 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string printReverseString = delegate(string content){
+                char[] charArray = content.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+
+            var productOfTwo = delegate(decimal x, decimal y){
+                return x * y;
+            }
+
+            var 
+
+
+            print(HelloWorld);
         }
     }
+    
 }
