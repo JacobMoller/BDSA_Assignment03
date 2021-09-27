2d468bd12a64552bf4331358f4126d2c637e72ae
JacobM <jacj@itu.dk>
2021-09-27 12:28:19 +0200

    Delegates code and tests

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,26 +1,14 @@
 ﻿using System;
 
-namespace Assignment3
+namespace BDSA2020.Assignment03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string printReverseString = delegate(string content){
-                char[] charArray = content.ToCharArray();
-                Array.Reverse(charArray);
-                return new string(charArray);
-            }
-
-            var productOfTwo = delegate(decimal x, decimal y){
-                return x * y;
-            }
-
-            //TEST HELO?
-
-
-            print(HelloWorld);
+            Console.WriteLine("Hello World!");
         }
     }
-    
+
 }
+
